91470cb11a3f7a14a39a1e0312558b3b10ce035b
Jesse op den Brouw <51508208+jesseopdenbrouw@users.noreply.github.com>
2026-03-30 11:50:06 +0200

    Update comments

--- rtl/thuas-riscv/rom.vhd
+++ rtl/thuas-riscv/rom.vhd
@@ -36,13 +36,14 @@
 
 -- This file contains the description of the ROM. The ROM is
 -- placed in mutable onboard RAM blocks and can be changed
--- by writing to it. A read takes one clock cycles for
--- instructions and two clock cycles for data. The ROM
--- contents is placed in file rom_image.vhd. ROM
--- can only be written on word boundaries when the bootloader
--- is synthesized. Instruction reads can only be on word
--- boundaries. Data read can be on byte, halfword and word
--- boundaries
+-- by writing to it. The ROM can only be written on word
+-- boundaries when the bootloader and/or the on-chip debugger
+-- is/are synthesized. A read takes one clock cycles for
+-- instructions and two clock cycles for data. A write takes
+-- one clock cycle. The ROM contents is placed in a file
+-- named rom_image.vhd. Instruction reads can only be on
+-- word boundaries. Data read can be on byte, halfword and
+-- word boundaries but must be natural aligned.
 -- Note: the core adds an extra buffer for memory operations.
 
 library ieee;
